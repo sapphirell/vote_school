81a233609aed8336695278d35f3683bbc62a625e
sapphirell <hentai@fantuanpu.com>
2019-05-21 22:50:42 +0800

    查询log接口

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
     "encoding/json"
     "fmt"
+    "github.com/tidwall/gjson"
+    "io/ioutil"
     "log"
     "net/http"
 
@@ -12,7 +14,7 @@ import (
 func schoolList(w http.ResponseWriter, req *http.Request) {
     w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
     w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
-    w.Header().Set("content-type", "application/json")             //返回数据格式是json
+    w.Header().Set("content-type", "application/json; charset=UTF-8")             //返回数据格式是json
 
     school := model.SchoolModel{}
     data,_ := school.GetSchoolRank()
@@ -26,14 +28,35 @@ func voteMySchool(w http.ResponseWriter, req *http.Request) {
     fmt.Fprintf(w, "voteMySchool")
 }
 
-func rangeSchool(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(w, "voteMySchool")
+func voteLog(w http.ResponseWriter, req *http.Request) {
+    w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
+    w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
+    w.Header().Set("content-type", "application/json; charset=UTF-8")  //返回数据格式是json
+
+    result,_ := ioutil.ReadAll(req.Body)
+    requestJson := string(result)
+    telPhone := gjson.Get(requestJson, "tel_phone").String()
+
+    fmt.Println(telPhone)
+    voteLog := model.VoteLogModel{}
+    data,_ := voteLog.GetVoteLog(telPhone)
+
+    fmt.Println(data)
+
+    resp ,_:= json.Marshal(data)
+    fmt.Fprintf(w, string(resp))
+    //jsonByte, _ := json.Marshal(data)
+    //
+    //n ,_ := fmt.Fprintf(w, string(jsonByte))
 }
 func main() {
+    fmt.Println("starting...")
     http.HandleFunc("/", schoolList)
     http.HandleFunc("/vote", voteMySchool)
-    err := http.ListenAndServe("localhost:8888", nil)
+    http.HandleFunc("/log", voteLog)
+    err := http.ListenAndServe(":8080", nil)
+    fmt.Println("success")
     if err != nil {
-        log.Fatal("ListenAndServe: ", err.Error())
+       log.Fatal("ListenAndServe: ", err.Error())
     }
 }
--- model/vote_log.go
+++ model/vote_log.go
@@ -1,62 +1,78 @@
 package model
-//
-//import (
-//    "database/sql"
-//    "fmt"
-//    "strconv"
-//)
-//
-//const table = "vep_cpr_orders"
-//
-//type CprOrdersModel struct {
-//    linker     DbLinker
-//    Db         *sql.DB
-//    Id         int
-//    SchoolName string
-//    MottoCh    string
-//    MottoEn    string
-//    Ticket     int
-//    Name       string
-//    Img        string
-//}
-//
-//func (m *CprOrdersModel) construct() {
-//    d := DbLinker{}
-//    d.Init()
-//    m.linker, m.Db = d, d.DB
-//}
-//
-//func (m *CprOrdersModel) Exec(query string, args ...interface{}) (sql.Result, error) {
-//    var res sql.Result
-//    var err error
-//    if args == nil {
-//        res, err = m.linker.DB.Exec(query)
-//    } else {
-//        res, err = m.linker.DB.Exec(query, args)
-//    }
-//
-//    return res, err
-//}
-//
-//func (m *CprOrdersModel) GetOrderDetail(orderId int64) error {
-//    m.construct()
-//
-//    queryString := "SELECT id,record_id,notify_url,ext FROM " + table + " WHERE id = ?"
-//    err := m.Db.QueryRow(queryString, orderId).Scan(&m.Id, &m.RecordId, &m.NotifyUrl, &m.Ext)
-//
-//    return err
-//}
-//
+
+import (
+    "database/sql"
+    "log"
+
+    //"fmt"
+    //"strconv"
+)
+
+const vote_log_table = "vote_log"
+
+type VoteLogModel struct {
+    linker DbLinker
+    Db     *sql.DB
+}
+
+type VoteLogRow struct {
+    Id       int    `json:"id"`
+    TelPhone string `json:"tel_phone"`
+    Sid      int    `json:"sid"`
+    Time     string `json:"time"` //time.Now().Format("2006-01-02 15:04:05")
+}
+
+func (m *VoteLogModel) constructVoteLogModel() {
+    d := DbLinker{}
+    d.Init()
+    m.linker, m.Db = d, d.DB
+}
+func (m *VoteLogModel) Exec(query string, args ...interface{}) (sql.Result, error) {
+    m.constructVoteLogModel()
+    var res sql.Result
+    var err error
+    if args == nil {
+        res, err = m.linker.DB.Exec(query)
+    } else {
+        res, err = m.linker.DB.Exec(query, args)
+    }
+
+    return res, err
+}
+
+//获取该手机号都投过谁
+func (m *VoteLogModel) GetVoteLog(telPhone string) ([]VoteLogRow,error) {
+    m.constructVoteLogModel()
+
+    queryString := "SELECT id,tel_phone,sid,time FROM " + vote_log_table + " WHERE tel_phone = ?"
+    rows, err := m.Db.Query(queryString, telPhone)
+    data := []VoteLogRow{}
+    i := 0
+    for rows.Next() {
+        row := VoteLogRow{}
+        err := rows.Scan(&row.Id, &row.TelPhone, &row.Sid, &row.Time)
+        if err != nil {
+            log.Fatal(err)
+        }
+        //log.Println(row)
+        data = append(data, row)
+        //data[i] = row
+        i++
+    }
+    return data, err
+
+}
+
 //func (m *CprOrdersModel) SaveOrderResult(renderTime float64, resultVideoUrl string, notifyResult int) {
-//    m.construct()
-//    var queryString string
+//   m.construct()
+//   var queryString string
 //
-//    queryString = "UPDATE " + table + " SET `render_time` = '" + strconv.FormatFloat(renderTime, 'f', -1, 64) + "' "
-//    queryString += " ,`result_video_url` = '" + resultVideoUrl + "' "
-//    queryString += " ,`notify_result` = '" + strconv.FormatInt(int64(notifyResult), 10) + "' "
+//   queryString = "UPDATE " + table + " SET `render_time` = '" + strconv.FormatFloat(renderTime, 'f', -1, 64) + "' "
+//   queryString += " ,`result_video_url` = '" + resultVideoUrl + "' "
+//   queryString += " ,`notify_result` = '" + strconv.FormatInt(int64(notifyResult), 10) + "' "
 //
-//    _, err := m.Db.Exec(queryString)
-//    if err != nil {
-//        fmt.Println(err)
-//    }
+//   _, err := m.Db.Exec(queryString)
+//   if err != nil {
+//       fmt.Println(err)
+//   }
 //}
